7d5c7fdbafe0f3d8aaa631d806ce0e17039648c0
Захар Рубанов <agcl3228@gmail.com>
2020-11-20 14:42:39 +0300

    Add Simple test generator

---
+++ TestGenerator/test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGenerator
+{
+    namespace Test{
+        class test
+        {
+            public int testProp { get; set; }
+            public int testFields;
+            public void TestMeth()
+            {
+
+            }
+            public class shit
+            {
+                int i;
+            }
+        }
+    }
+    namespace Huo
+    {
+        class hias
+        {
+            private void TestMeth()
+            {
+
+            }
+        }
+    }
+}
